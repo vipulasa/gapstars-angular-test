ae2db14fc49085e324ab4a0d0d022fcc53a0ccef
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2025-04-17 14:34:10 +0530

    Refactor add-todo component with enhanced method comments.
    Added detailed JSDoc comments for better clarity and understanding of key methods, including `isChecked`, `toggleDependency`, and `addTask`. Minor adjustments to code style for consistency.

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { TodoService } from '../services/todo.service';
-import { Priority, Recurrence, TodoModel } from '../models/todo.model';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {TodoService} from '../services/todo.service';
+import {Priority, Recurrence, TodoModel} from '../models/todo.model';
 import {FormsModule, NgForm} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 
@@ -24,16 +24,31 @@ export class AddTodoComponent {
 
   dependencies: number[] = [];
 
-  constructor(public todoService: TodoService) {}
+  constructor(public todoService: TodoService) {
+  }
 
   @Output() taskAdded = new EventEmitter<void>();
 
   @Output() closed = new EventEmitter<void>();
 
+  /**
+   * Determines whether the given ID exists in the dependencies list.
+   *
+   * @param {number} id - The ID to check for inclusion in the dependencies.
+   * @return {boolean} True if the ID is included in the dependencies, otherwise false.
+   */
   isChecked(id: number): boolean {
     return this.dependencies.includes(id);
   }
 
+  /**
+   * Toggles the inclusion of a dependency ID in the dependencies list.
+   * If the ID is already in the list, it will be removed.
+   * If the ID is not in the list, it will be added.
+   *
+   * @param {number} id - The ID of the dependency to be toggled in the list.
+   * @return {void}
+   */
   toggleDependency(id: number): void {
     this.dependencies.includes(id)
       ? this.dependencies = this.dependencies.filter(d => d !== id)
@@ -41,9 +56,14 @@ export class AddTodoComponent {
   }
 
   /**
+   * Adds a new task to the task list based on the form input.
+   * Resets the form after successfully adding the task.
    *
+   * @param {NgForm} form - The reactive form containing task details.
+   *                       Must be valid to add the task.
+   * @return {void}
    */
-  addTask(form: NgForm) {
+  addTask(form: NgForm): void {
 
     if (!form || form.invalid) return;
 
